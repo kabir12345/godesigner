cf304a93d6b473e49a568dff3d1cdeb5f1957bdf
Kabir Jaiswal <kabirjaiswal30@gmail.com>
2023-07-08 20:25:49 -0400

    bug fix

--- app.py
+++ app.py
@@ -1,39 +1,23 @@
 # import libraries
-from flask import Flask, request, app, jsonify, url_for, render_template, send_file
+from flask import Flask, request, app, url_for, render_template
 from utils.model import model_intial, generate_output
-import pandas as pd
-import matplotlib.pyplot as plt
-from io import BytesIO
 
 
 # intitalising flask app
-app = Flask(__name__,static_folder='static', template_folder='templates')
+app = Flask(__name__)
 
-# loading model
-
-
-
+model_id = None
+pipe = None
 # index.html route
 @app.route("/")
 def homepage():
     return render_template("index.html")
 
-
-# api route
-# @app.route("/generate_image_api", methods=["POST"])
-# def generate_image_api():
-#     data = request.json["data"]["prompt"]
-#     gen_image = generate_output(data, pipe)
-#     byte_io = BytesIO()
-#     gen_image.save(byte_io, "PNG")
-#     byte_io.seek(0)
-#     return send_file(byte_io, mimetype="image/png")
-
-
 @app.route("/generate", methods=["POST"])
 def generate():
     data = request.form.get('prompt')
-    model_id, pipe = model_intial()
+    if model_id is None or pipe is None:
+        model_id, pipe = model_intial()
     gen_image = generate_output(data, pipe)
     filename = 'static/generated_image.png'
     gen_image.save(filename)
@@ -48,3 +32,4 @@ def health_check():
 
 if __name__ == "__main__":
     app.run()
+
--- utils/model.py
+++ utils/model.py
@@ -1,21 +1,6 @@
 # import libraries
 import torch
-import pandas as pd
-from torchvision.io import read_image
-from PIL import Image
 from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
-from diffusers import UNet2DConditionModel
-from torch.optim import AdamW
-from transformers import get_scheduler
-import pickle
-from utils.dataset import dataloader
-
-# create data loader
-# master_dir='/content/drive/MyDrive/InteriorGenProj/'
-# img_path='/content/drive/MyDrive/InteriorGenProj/Interior_inference/Interior_dataset/intimages/'
-# text_path='/content/drive/MyDrive/InteriorGenProj/Interior_inference/Interior_dataset/text_data/'
-
-# train_dataloader,test_dataloader=dataloader(img_path,text_path)
 
 
 # intialising model
@@ -25,8 +10,7 @@ def model_intial():
     pipe = StableDiffusionPipeline.from_pretrained(
         model_id, scheduler=scheduler, torch_dtype=torch.float32
     )
-    if torch.cuda.is_available():
-        pipe = pipe.to("cuda")
+    pipe = pipe.to("cuda")
     return model_id, pipe
 
 
@@ -34,22 +18,3 @@ def model_intial():
 def generate_output(prompt, pipe):
     image = pipe(prompt).images[0]
     return image
-
-
-# finetuning the model
-def fine_tuning_model(model_id):
-    model = UNet2DConditionModel.from_pretrained(model_id, subfolder="unet")
-    optimizer = AdamW(model.parameters(), lr=1e-4)
-    device = torch.device("gpu") if torch.cuda.is_available() else torch.device("cpu")
-    model.to(device)
-    num_epochs = 3
-    # num_training_steps = num_epochs * len(train_dataloader)
-    # lr_scheduler = get_scheduler(
-    # name="linear", optimizer=optimizer, num_warmup_steps=0, num_training_steps=num_training_steps)
-    # fine-tuning code
-    return model
-
-
-# saving the model
-def save_model(model, file_loc):
-    pickle.dump(model, open(file_loc, "wb"))
